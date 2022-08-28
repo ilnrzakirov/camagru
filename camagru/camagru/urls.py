ebe18eb8068b7523518b15d7ff3654fdc2a712ce
Ilnur <ilnrzakirov@gmail.com>
2022-08-28 15:02:48 +0300

    add base and nav html bootstrap base.css and routing urls

--- camagru/camagru/urls.py
+++ camagru/camagru/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from camagru.camagru import settings
+
 from django.conf.urls.static import static
+
+from camagru import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('webcam.urls'))
 ]
 
 if settings.DEBUG:
