945aa9b45eb05f30b3055769fa3a47e1213b533c
Ilnur <ilnrzakirov@gmail.com>
2022-09-12 14:28:00 +0300

    add str method for model

--- webcam/models.py
+++ webcam/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
