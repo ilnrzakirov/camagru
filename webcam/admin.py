10a5a19bf05e9c66f4d4466de230206c91e75db5
Ilnur <ilnrzakirov@gmail.com>
2022-09-19 10:04:32 +0300

    Посты зареганы в админке

--- webcam/admin.py
+++ webcam/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from webcam.models import ImageFilter, Post, Picture
 
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ["user", "pub_date", "comment", "image"]
+
 admin.site.register(ImageFilter)
 admin.site.register(Post)
 admin.site.register(Picture)
